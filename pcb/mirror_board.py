b11f07538832eabe054736738fbdd47e5917623f
Herman Lundkvist <herman.lundkvist@gmail.com>
2021-04-04 18:09:47 +0200

    Mirror script works, but not a good idea.

--- pcb/mirror_board.py
+++ pcb/mirror_board.py
@@ -7,19 +7,32 @@ class MirrorBoard(pcbnew.ActionPlugin):
         self.description = "N/A"
         self.origin = pcbnew.wxPoint(0, 0)
 
-    def Mirror(self, point, orientation):
+    def MirrorPoint(self, point, orientation, flipped):
         new_point = pcbnew.wxPoint(self.origin.x - (point.x - self.origin.x), point.y);
-        new_orientation = 180 - orientation
+        new_orientation = orientation
+        if flipped:
+            new_orientation = -orientation
+        else:
+            new_orientation = 180 - orientation
+
         return (new_point, new_orientation)
 
     def MirrorLine(self, start, end):
-        return (pcbnew.wxPoint(-end.x, end.y), pcbnew.wxPoint(-start.x, start.y))
+        return (pcbnew.wxPoint(-start.x, start.y), pcbnew.wxPoint(-end.x, end.y))
         
     def Run(self):
         board = pcbnew.GetBoard()
-        for module in board.GetModules():
-            pos, orient = self.Mirror(module.GetPosition(), module.GetOrientationDegrees())
-            module.SetPosition(pos)
-            module.SetOrientationDegrees(orient)
+        for item in board.GetModules():
+            pos, orient = self.MirrorPoint(item.GetPosition(), item.GetOrientationDegrees(), item.IsFlipped())
+            item.SetPosition(pos)
+            item.SetOrientationDegrees(orient)
+        for item in board.GetTracks():
+            if type(item) is pcbnew.VIA:
+                pos, _ = self.MirrorPoint(item.GetPosition(), 0, False)
+                item.SetPosition(pos)
+            elif type(item) is pcbnew.TRACK:
+                start, end = self.MirrorLine(item.GetStart(), item.GetEnd())
+                item.SetStart(start)
+                item.SetEnd(end)
 
 MirrorBoard().register()
